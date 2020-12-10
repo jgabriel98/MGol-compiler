c45235806babee4b00263fc443a70d4197f53080
João Gabriel <jgabsfernandes@gmail.com>
2020-12-10 00:31:13 -0300

    partial commit too keep file track

--- src/Automata.h
+++ src/Automata.h
@@ -1,26 +1,19 @@
 #ifndef AUTOMATA_HEADER
 #define AUTOMATA_HEADER
 
-#include <vector>
 #include <unordered_map>
 #include <unordered_set>
 
 #include "utils.h"
 
-
-using namespace std;
-
-
 class Automata {
+   private:
+	std::unordered_map<std::pair<int, char>, int, hash_pair> transitions;
+	std::unordered_set<int> final_states;
+	int s0;			 // initial state
+	int rej_s = -1;	 // rejection state
 
-private:
-	unordered_map<pair<int, char>, int, hash_pair> transitions;
-	unordered_set<int> final_states;
-	int s0; // initial state
-	int rej_s = -1;	//rejection state
-
-public:
-
+   public:
 	Automata(int start);
 	Automata(int start, int rejection);
 
@@ -30,12 +23,12 @@ public:
 
 	int process_char(int s, char c);
 
-	int process_string(const string &str);
+	int process_string(const std::string &str);
 
 	bool is_final_state(int state);
 
-	inline int initial_state() {return s0;}
-	inline int rejection_state() {return rej_s;}
+	inline int initial_state() { return s0; }
+	inline int rejection_state() { return rej_s; }
 };
 
 #endif
